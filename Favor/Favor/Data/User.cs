f9a0fd0534576a6f7164b4dea6ab84bb46f4025a
nikolayoyo <nikola.yy@icloud.com>
2017-04-19 19:38:04 +0300

    Added PayOff.cs and edited the user model

--- Favor/Favor/Data/User.cs
+++ Favor/Favor/Data/User.cs
@@ -4,10 +4,27 @@
     using System.Threading.Tasks;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
-    
+    using System.Collections.Generic;
+
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class User : IdentityUser
     {
+        public User()
+        {
+            this.MyFavors = new List<string>();
+            this.PendingFavors = new List<string>();
+            this.AccomplishedFavors = new List<string>();
+            this.SentFavors = new List<string>();
+        }
+
+        public ICollection<string> MyFavors { get; set; }
+
+        public ICollection<string> PendingFavors { get; set; }
+
+        public ICollection<string> AccomplishedFavors { get; set; }
+
+        public ICollection<string> SentFavors { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
