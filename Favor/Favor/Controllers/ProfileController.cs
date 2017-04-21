68d47e6b3398c5a92bf5a94a8cd29482dd65f7ee
simonradev <simonradev@gmail.com>
2017-04-21 22:29:34 +0300

    Edited the Profile

--- Favor/Favor/Controllers/ProfileController.cs
+++ Favor/Favor/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Favor.Data;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,21 @@ namespace Favor.Controllers
 {
     public class ProfileController : Controller
     {
-        
         [HttpGet]
         public ActionResult Profile()
         {
-            return View();
+            var db = new FavorDbContext();
+
+            var currUserId = this.User.Identity.GetUserId();
+
+            var userToView = db.Users.FirstOrDefault(u => u.Id == currUserId);
+
+            if (userToView == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(userToView);
         }
     }
 }
