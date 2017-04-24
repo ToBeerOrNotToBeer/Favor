083dd48195dba89c927f4a9d753e9c15069e3c2f
simonradev <simonradev@gmail.com>
2017-04-25 02:20:38 +0300

    Admin Controllers/AdminController.cs test

--- Favor/Favor/Controllers/AdminController.cs
+++ Favor/Favor/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Favor.Controllers
 {
@@ -11,7 +12,38 @@ namespace Favor.Controllers
         [HttpGet]
         public ActionResult AdminPanel()
         {
-            return View();
+            //if (!this.User.IsInRole("Admin"))
+            //{
+            //    return RedirectToAction("Index", "Home");
+            //}
+
+            var db = new Favor.Data.FavorDbContext();
+
+            var allUsers = db.Users.ToList();
+
+            return View(allUsers);
+        }
+
+        [Authorize]
+        public ActionResult DeleteUser(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("AdminPanel");
+            }
+
+            var db = new Favor.Data.FavorDbContext();
+
+            var userToRemove = db.Users.Find(id);
+
+            if (userToRemove == null)
+            {
+                return RedirectToAction("AdminPanel");
+            }
+
+            db.Users.Remove(userToRemove);
+
+            return RedirectToAction("AdminPanel");
         }
     }
 }
