f54839cc7531263d4054e84ced9558d6a3aeea9c
simonradev <simonradev@gmail.com>
2017-04-24 22:53:52 +0300

    asd

---
+++ Favor/Favor/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Favor.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult AdminPanel()
+        {
+            return View();
+        }
+    }
+}
