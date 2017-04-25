38aa6e164f2de96cbd08b3b96de633f1c713bb3b
nikolayoyo <nikola.yy@icloud.com>
2017-04-25 16:26:07 +0300

    Added AccomplishedFavorDetails

---
+++ Favor/Favor/Controllers/AccomplishedFavorController.cs
@@ -0,0 +1,35 @@
+﻿using Favor.Data;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Favor.Controllers
+{
+    public class AccomplishedFavorController : Controller
+    {
+        // GET: AccomplishedFavor
+        public ActionResult AccomplishedFavorDetails(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var db = new FavorDbContext();
+            var userId = this.User.Identity.GetUserId();
+            var user = db.Users.Find(userId);
+            var accomplishedFavor = user.AccomplishedFavors.FirstOrDefault(f => f.Id == id);
+            if (accomplishedFavor == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+
+            return View(accomplishedFavor);
+        }
+    }
+}
