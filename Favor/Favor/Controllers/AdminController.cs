8394d092fc76bb19dc0cacc715c99303c8e1a51b
simonradev <simonradev@gmail.com>
2017-04-27 18:12:46 +0300

    Nothing much

--- Favor/Favor/Controllers/AdminController.cs
+++ Favor/Favor/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -12,40 +15,16 @@ namespace Favor.Controllers
         [HttpGet]
         public ActionResult AdminPanel()
         {
-            //if (!this.User.IsInRole("Admin"))
-            //{
-            //    return RedirectToAction("Index", "Home");
-            //}
+            if (!this.User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
             var db = new Favor.Data.FavorDbContext();
 
             var allUsers = db.Users.ToList();
 
             return View(allUsers);
-        }
-
-        [Authorize]
-        public ActionResult DeleteUser(int? integerIdToDelete)
-        {
-            if (integerIdToDelete == null)
-            {
-                return RedirectToAction("AdminPanel");
-            }
-
-            var db = new Favor.Data.FavorDbContext();
-
-            var userToRemove = db.Users.FirstOrDefault(u => u.IntegerId == integerIdToDelete);
-
-            if (userToRemove == null)
-            {
-                return RedirectToAction("AdminPanel");
-            }
-
-            db.Users.Remove(userToRemove);
-
-            db.SaveChanges();
-
-            return RedirectToAction("AdminPanel");
         }
     }
 }
